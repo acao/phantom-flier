bdb75f350b9cc16e160999c133417ea61d201de7
Richard Schulte <richard.j.schulte@gmail.com>
2015-02-05 13:23:33 -0500

    works now, logging, simplified functional pattern

--- bin/phantompdf.js
+++ bin/phantompdf.js
@@ -16,47 +16,19 @@ var
 config.defaults = config.options = require('../defaults.json');
 
 
-phantomFlier.options.buildConfig(config, process, function (err, options) {
-  if (err) {
-      console.log("Error while processing " + file + ":");
-      console.log(err);
-    } else {
-      if (phantomFlier.options.isValid(options)) {
-        var render = phantom(),
-        outputpath = [process.cwd(), config.paths.dest].join("/");
-        render(config.paths.source, config.options)
-        .pipe(fs.createWriteStream(outputpath));
-      } else {
-        console.log(html);
-      }
-    }
-
-});
+config = phantomFlier.options.buildConfig(config, process);
 
 
+var render = phantom(),
+outputpath = [process.cwd(), config.paths.dest].join("/");
 
+// Render with phantom, output to config'd path
 
-
-
-// fs.writeFile(outputpath, 'Example Output', function (err) {
-//   if (err) throw err;
-// });
-
-
-      // .pipe();
-// process.exit(0);
-
-// _.each(argv._, function (file) {
-//   phantompdf.processPage(file, argv, function (err, html) {
-//     if (err) {
-//       console.log("Error while processing " + file + ":");
-//       console.log(err);
-//     } else {
-//       if (argv.output) {
-//         console.log("Saved to " + argv.output + ".");
-//       } else {
-//         console.log(html);
-//       }
-//     }
-//   });
-// });
+render(config.paths.source, config.options)
+  .pipe(fs.createWriteStream(outputpath))
+  .on('error', function(e){
+    utils.logger.log('error', 'whoops');
+    console.log(e);
+  }).on('end', function(){
+    utils.logger.log('info', 'win!');
+  });
